910fd0c85cf3d84569b59e538d926b6f71c1cea1
Matt Bailey <baimy017@mymail.unisa.edu.au>
2021-05-30 18:35:21 +0930

    Commit #6
    Menu functionality implemented and assets created.
    
    Ready to merge with the main branch.
    
    Signed-off-by: Matt Bailey <baimy017@mymail.unisa.edu.au>

--- Game_LibGDXProject/core/src/com/mygdx/game/GameScreen.java
+++ Game_LibGDXProject/core/src/com/mygdx/game/GameScreen.java
@@ -1,4 +1,55 @@
 package com.mygdx.game;
 
-public class GameScreen {
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+
+    MyGdxGame game;
+
+    public enum Difficulty{EASY, MEDIUM, HARD};
+
+    private Difficulty difficulty;
+
+    public GameScreen(MyGdxGame game){
+        this.game = game;
+    }
+
+    public void create(Difficulty difficulty){
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
