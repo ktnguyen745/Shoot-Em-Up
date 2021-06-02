f4cfd11c6923b7a36207bc88045e227b9e40ee02
Matt Bailey <baimy017@mymail.unisa.edu.au>
2021-06-02 11:07:05 +0930

    BulletManager Commit #1
    Implementation of BulletManager class, next step is to integrate it with the existing code.

---
+++ Game_LibGDXProject/core/src/com/mygdx/game/BulletManager.java
@@ -0,0 +1,85 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class BulletManager {
+
+    public enum Type {NONE, BULLET};
+    private static final int max_bullets = 128;
+
+    private Texture bulletTexture;
+    private float bulletLifespan;
+
+    Type[] bullets = new Type[max_bullets];
+    Vector2[] position = new Vector2[max_bullets];
+    Vector2[] velocity = new Vector2[max_bullets];
+    float[] lifespan = new float[max_bullets];
+
+    public BulletManager(String bulletTexture, float bulletLifespan){
+        this.bulletTexture = new Texture(Gdx.files.internal(bulletTexture));
+        this.bulletLifespan = bulletLifespan;
+    }
+
+    public void init(){
+        for(int i = 0; i < max_bullets; i++){
+            bullets[i] = Type.NONE;
+            position[i] = new Vector2();
+            velocity[i] = new Vector2();
+        }
+    }
+
+    public int spawnBullet(Type type, float posX, float posY, float velX, float velY){
+        // If no bullet is to be spawned,return a fail result
+        if(type == Type.NONE) return -1;
+
+        // Find empty spot in bullets array
+        int index = -1;
+        for(int free = 0; free < max_bullets; free++){
+            if(bullets[free] == Type.NONE){
+                index = free;
+                break;
+            }
+        }
+        // If no free spots were found
+        if(index < 0) return -1;
+
+        // If a free spot was found
+        bullets[index] = type;
+        position[index].x = posX;
+        position[index].y = posY;
+        velocity[index].x = velX;
+        velocity[index].y = velY;
+        lifespan[index] = bulletLifespan;
+
+        return index;
+    }
+
+    public void update(float dt){
+        for(int i = 0; i < max_bullets; i++){
+            if(bullets[i] != Type.NONE){
+                lifespan[i] -= dt;
+                if(lifespan[i] < 0){
+                    bullets[i] = Type.NONE;
+                } else {
+                    position[i].mulAdd(velocity[i], dt);
+                }
+            }
+        }
+    }
+
+    public void render(SpriteBatch batch){
+        for(int i = 0; i < max_bullets; i++){
+            if(bullets[i] != Type.NONE){
+                batch.draw(bulletTexture, position[i].x, position[i].y);
+            }
+        }
+    }
+
+    public void dispose(){
+        bulletTexture.dispose();
+    }
+
+}
